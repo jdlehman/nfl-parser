8c095c3765f8ac004372dba694d192d3c13b91f6
Jonathan Lehman <jonathan.lehman91@gmail.com>
2014-08-22 20:21:11 -0400

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject nfl-parser "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main ^:skip-aot nfl-parser.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ test/nfl_parser/core_test.clj
@@ -0,0 +1,7 @@
+(ns nfl-parser.core-test
+  (:require [clojure.test :refer :all]
+            [nfl-parser.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
