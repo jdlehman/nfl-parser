117ba88715d6f0d5a688cfade651341dd424cb0e
Jonathan Lehman <jonathan.lehman91@gmail.com>
2014-08-23 22:56:20 -0400

    Create nfl.com parser using enlive
    - Get all team and player data from nfl.com

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [enlive "1.1.1"]]
   :main ^:skip-aot nfl-parser.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/nfl_parser/core.clj
+++ src/nfl_parser/core.clj
@@ -1,7 +1,39 @@
 (ns nfl-parser.core
+  (:require [net.cgrand.enlive-html :as html])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def base-url "http://www.nfl.com")
+
+(defn fetch-url [url]
+  (html/html-resource (java.net.URL. url)))
+
+(defn scrape-team-links []
+   (html/select (fetch-url (str base-url "/teams"))
+                [(html/attr-starts :href "/team")]))
+
+(defn get-roster-href [col]
+  (clojure.string/replace (str base-url
+                (get-in (into {} col) [:attrs :href]))
+                #"profile" "roster"))
+
+(defn get-links []
+  (into #{}
+        (map get-roster-href (scrape-team-links))))
+
+(defn scrape-roster [url]
+  (map html/text
+       (html/select (fetch-url url)
+                    [:div#team-stats-wrapper :table#result :tbody :tr])))
+
+(defn scrape-all-rosters []
+  (map scrape-roster (get-links)))
+
+(defn print-player [player-data]
+  (println (str (clojure.string/replace player-data #"\n" "\t") "\n")))
+
+(defn get-team [team-data]
+  (map print-player team-data))
+
+(defn -main []
+  (time (doall
+          (map get-team (scrape-all-rosters)))))
